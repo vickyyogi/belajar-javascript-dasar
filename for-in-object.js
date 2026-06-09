d04d42654f0900a33d1728c99f9d697c1bc91ec4
vickyyogi <pratamakyogi@gmail.com>
2026-06-09 10:15:45 +0700

    adding for in loops

--- for-in-object.js
+++ for-in-object.js
@@ -1,32 +1,44 @@
-const dataUser = {
-    "user101" : {
-        nama: "Dian",
-        hobi: ["memancing","berenang"],
-        pekerjaan: "penyanyi",
-        alamat:{
-            desa:"Klampok",
-            kec:"Singosari",
-            kota:"Malang"
-        }
-    },
-    "user102" : {
-        nama: "Juna",
-        hobi: ["memasak","berkendara"],
-        pekerjaan: "koki",
-        alamat:{
-            desa:"Muara Baru",
-            kec:"Jonggol",
-            kota:"Jakarta Barat"
-        }
-    }
-}
-
+console.log("___________-___________")
+console.log("For in dalam Object");
+console.log("_______________________")
+//contoh mengambil nilai dari object
+//contoh object
 const mobil = {
     merk: 'toyota',
     type: "avanza",
     tahun: 2019
 }
 
+//variabel "data" bisa diubah dengan kata apa saja yang mewakili
+//kemudian akan kembali di tampikan menggunakan log
 for (const data in mobil) {
     console.log(data +" : "+ mobil[data] )
+}
+
+
+console.log("___________-___________")
+console.log("For in dalam Object Bersarang");
+console.log("_______________________")
+
+//contoh penggunaan pada object bersarang
+const users = {
+    nama: "Yuni",
+    age: 23,
+    status: "single",
+    alamat: {
+        desa: "mulyorejo",
+        kecamatan: "blimbing",
+        kota: "malang"
+    }
+}
+
+//menggunakan pengulangan bertingkat
+for (const data in users){
+    if(typeof users[data] === "object"){
+        for (const dataDalam in users[data]){
+            console.log(dataDalam + " : " + users[data][dataDalam]);
+        }
+    }else{
+        console.log(data + " : " + users[data]);
+    }
 }
